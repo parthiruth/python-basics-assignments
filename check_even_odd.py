10c1606a0a77f054494ac9c6d467a4d3e1731bfe
parthiruth <msparthiruth@gmail.com>
2026-04-14 16:30:27 +0530

    Check even odd number

---
+++ check_even_odd.py
@@ -0,0 +1,5 @@
+check_number = int (input("Enter The Number :"))
+if check_number%2 ==0 :
+    print ("The Number Is Even")
+else :
+    print("The Number Is Odd:")
--- check_positive_number.py
+++ check_positive_number.py
@@ -1,5 +1,5 @@
 check_number = int (input("Enter The Number :"))
-if check_number%2 ==0 :
+if check_number >= 0 :
     print ("The Number Is Positive")
 else :
     print("The Number Is Negative:")
